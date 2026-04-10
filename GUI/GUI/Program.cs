1743db9dd6d134c8f94c0f4501be2e4d43887477
oscar jonatan vasquez rivera <vasquezriveraoscarjonatan.com>
2026-04-09 22:28:30 -0600

    Parte visual

---
+++ GUI/GUI/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+namespace GUI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            // Aquí decides qué formulario abre primero
+            Application.Run(new Form3());
+        }
+    }
+}
