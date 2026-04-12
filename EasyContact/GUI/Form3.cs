99c30386df4cc8eae35cd2cd73796bab62bd9dcd
Lisbeth Hernandez <lishernandez920@gmail.com>
2026-04-12 10:16:22 -0600

    Update Form3.cs
    Se agrega codigo de navegacion entre vistas

--- EasyContact/GUI/Form3.cs
+++ EasyContact/GUI/Form3.cs
@@ -29,7 +29,11 @@ namespace GUI
 
         private void button1_Click(object sender, EventArgs e)
         {
-          
-        }
+
+			Form2 f2 = new Form2();
+			f2.Show();
+			this.Hide();
+
+		}
     }
 }
