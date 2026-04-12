6ad26d00e788689b3f37859fe9928b6d25a2a7cb
oscar jonatan vasquez rivera <vasquezriveraoscarjonatan.com>
2026-04-12 15:33:34 -0600

    Agregando navegacion entre paginas

--- EasyContact/GUI/Form2.cs
+++ EasyContact/GUI/Form2.cs
@@ -1,11 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 
@@ -25,6 +18,7 @@ namespace GUI
 
         private void button2_Click(object sender, EventArgs e)
         {
+
 
         }
 
