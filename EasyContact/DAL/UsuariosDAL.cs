e7a61afd4232fa33372f45284be414dad18ddd2d
Lisbeth Hernandez <lishernandez920@gmail.com>
2026-04-24 23:26:23 -0600

    Codigo de usuarioDAL
    Implement login functionality to authenticate users.

--- EasyContact/DAL/UsuariosDAL.cs
+++ EasyContact/DAL/UsuariosDAL.cs
@@ -1,15 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using EL;
+using System;
+using System.Data.SqlClient;
 
 namespace DAL
 {
-
     public class UsuariosDAL
     {
+        public UsuariosEL Login(string usuario, string clave)
+        {
+            using (SqlConnection cn = new ConexionDB().ObtenerConexion())
+            {
+                cn.Open();
+
+                string query = "SELECT * FROM UsuariosEL WHERE Usuario=@usuario AND Clave=@clave";
+                SqlCommand cmd = new SqlCommand(query, cn);
+                cmd.Parameters.AddWithValue("@usuario", usuario);
+                cmd.Parameters.AddWithValue("@clave", clave);
+
+                SqlDataReader dr = cmd.ExecuteReader();
+                if (dr.Read())
+                {
+                    return new UsuariosEL
+                    {
+                        IdUsuario = Convert.ToInt32(dr["IdUsuario"]),
+                        Usuario = dr["Usuario"].ToString(),
+                        Clave = dr["Clave"].ToString(),
+                        Rol = dr["Rol"].ToString()
+                    };
+                }
+            }
+
+            // Si no se encontró usuario
+            return null;
+        }
     }
-
-
-}
+}﻿
